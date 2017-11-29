05bbe3a32b1257cb8708f998576d45772f845f00
kuwali <kustiawanto.halim@gmail.com>
2017-11-30 00:41:09 +0700

    Add publisher

---
+++ uas/publisher.js
@@ -0,0 +1,18 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'EX_PING';
+    var msg = {
+      action: 'ping',
+      npm: '1406543763',
+      ts: new Date()
+    };
+
+    ch.assertExchange(ex, 'fanout', {durable: false});
+    ch.publish(ex, '', new Buffer(JSON.stringify(msg)));
+    console.log(" [x] Sent %s", msg);
+  });
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
