448c9f8ea8c8ece8f1ce39be52259bb030932799
kuwali <kustiawanto.halim@gmail.com>
2017-11-30 01:43:31 +0700

    Add subscriber

---
+++ uas/subscriber.js
@@ -0,0 +1,18 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'EX_PING';
+
+    ch.assertExchange(ex, 'fanout', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      ch.bindQueue(q.queue, ex, '');
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [x] %s", msg.content.toString());
+      }, {noAck: true});
+    });
+  });
+});
