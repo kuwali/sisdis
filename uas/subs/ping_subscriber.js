cfde88af11d1cde1d5b2e6823be7a418d4a162bb
kuwali <kustiawanto.halim@gmail.com>
2017-12-12 01:53:13 +0700

    Update dir

---
+++ uas/subs/ping_subscriber.js
@@ -0,0 +1,20 @@
+var amqp = require('amqplib/callback_api');
+var store = require('../model/quorum');
+
+amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'EX_PING';
+
+    ch.assertExchange(ex, 'fanout', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(" [*] Waiting for ping in %s. To exit press CTRL+C", q.queue);
+      ch.bindQueue(q.queue, ex, '');
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [P] %s", msg.content.toString());
+        store.save(JSON.parse(msg.content));
+      }, {noAck: true});
+    });
+  });
+});
