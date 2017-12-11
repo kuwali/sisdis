2d4f88946df6b593391d7d9c057342fe9328ec73
kuwali <kustiawanto.halim@gmail.com>
2017-12-12 02:03:29 +0700

    Add files

---
+++ uas/subs/get_total_saldo_subscriber.js
@@ -0,0 +1,20 @@
+var amqp = require('amqplib/callback_api');
+var store = require('../model/quorum');
+
+amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'EX_GET_TOTAL_SALDO';
+
+    ch.assertExchange(ex, 'direct');
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(" [*] Waiting for getsaldo in %s. To exit press CTRL+C", q.queue);
+      ch.bindQueue(q.queue, ex, 'RESP_1406543763');
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [S] %s", msg.content.toString());
+        // store.save(JSON.parse(msg.content));
+      }, {noAck: true});
+    });
+  });
+});
