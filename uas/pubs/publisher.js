3ccfaa1de213a0bb6cabbb066373e6ce70a9ad62
kuwali <kustiawanto.halim@gmail.com>
2017-12-12 02:06:12 +0700

    Re-submit ping

--- uas/pubs/publisher.js
+++ uas/pubs/publisher.js
@@ -1,43 +1,23 @@
 const amqp = require('amqplib/callback_api');
 
-// (function loop() {
-//   setTimeout(function() {
-//     amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
-//       conn.createChannel(function(err, ch) {
-//         var ex = 'EX_PING';
-//         var msg = {
-//           action: 'ping',
-//           npm: '1406543763',
-//           ts: new Date().toLocaleString('en-US', { hour12: false })
-//         };
+(function loop() {
+  setTimeout(function() {
+    amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
+      conn.createChannel(function(err, ch) {
+        var ex = 'EX_PING';
+        var msg = {
+          action: 'ping',
+          npm: '1406543763',
+          ts: new Date().toLocaleString('en-US', { hour12: false })
+        };
   
-//         ch.assertExchange(ex, 'fanout', {durable: false});
-//         ch.publish(ex, '', new Buffer(JSON.stringify(msg)));
-//         console.log(" [x] Sent %s", JSON.stringify(msg));
-//       });
-  
-//       setTimeout(function() { conn.close() }, 500);
-//     });
-//     loop();
-//   }, 5000);
-// })();
-
-amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var ex = 'EX_PING';
-    var msg = {
-      action: 'ping',
-      npm: '1406543763',
-      ts: new Date().toLocaleString('en-US', { hour12: false })
-    };
-
-    ch.assertExchange(ex, 'fanout', {durable: false});
-    
-    while(1) {
-      setTimeout(function() {
+        ch.assertExchange(ex, 'fanout', {durable: false});
         ch.publish(ex, '', new Buffer(JSON.stringify(msg)));
         console.log(" [x] Sent %s", JSON.stringify(msg));
-      }, 5000);
-    };
-  });
-});
+      });
+  
+      setTimeout(function() { conn.close() }, 500);
+    });
+    loop();
+  }, 5000);
+})();
